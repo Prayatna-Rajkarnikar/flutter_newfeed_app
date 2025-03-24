8ee6cc40819aff7f3361e7fb105c90725b7cfdbc
Prayatna Rajakrnikar <prayatna.rajkarnikar@gmail.com>
2025-03-24 14:41:26 +0545

    feat: news feed screen, get all news

--- lib/screens/newsfeed_screen.dart
+++ lib/screens/newsfeed_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:newsfeed_app/model/news_post.dart';
+import 'package:newsfeed_app/service/api_service.dart';
 
-class NewsfeedScreen extends StatelessWidget {
+class NewsfeedScreen extends StatefulWidget {
   const NewsfeedScreen({super.key});
 
   @override
+  State<NewsfeedScreen> createState() => _NewsfeedScreenState();
+}
+
+class _NewsfeedScreenState extends State<NewsfeedScreen> {
+  late Future<List<NewsPost>> _newsPosts;
+
+  @override
+  void initState() {
+    super.initState();
+    _newsPosts = ApiService().getNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Newsfeed")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Newsfeed")),
+      body: FutureBuilder(
+        future: _newsPosts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error loading news"));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final post = snapshot.data![index];
+                return Card(
+                  margin: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Text(post.title), Text(post.body)],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
